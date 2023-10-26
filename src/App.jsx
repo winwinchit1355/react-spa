da77112140a773447c3543ea6d0bea34cb53aa6a
winwinchit1355 <winwinchit1355@gmail.com>
2023-10-26 22:02:01 +0630

    first init

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React,{useState} from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Container } from './styles/Shares';
+import { ThemeProvider } from 'styled-components';
+import {GlobalCss} from './styles/GlobalCss';
+import Nav from './components/Nav';
+
+const theme = {
+  color:{
+    primary:"aliceblue",
+  }
+}
+
+function App() {
+
+  return (
+    <ThemeProvider theme={theme} >
+      <GlobalCss />
+      <Container>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Nav />} />
+          </Routes>
+        </Router>
+      </Container>
+    </ThemeProvider>
+    
+  )
+}
+
+export default App
---
+++ src/styles/GlobalCss.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalCss = createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+    }
+    a{
+        text-decoration:none;
+    }
+    ul{
+        list-style-type:none;
+    }
+`;
