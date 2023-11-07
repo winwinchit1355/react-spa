70bed262dfb51df3800140900d02ffde7d9a60cd
winwinchit1355 <winwinchit1355@gmail.com>
2023-11-07 17:15:56 +0630

    fix bug

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,12 @@ export default function Login() {
     <div>
       <Nav />
       <h1>Login</h1>
+      <div>
+        <form action="">
+            <input type="tel" name="phone" />
+            <input type="password" name="pa" />
+        </form>
+      </div>
     </div>
   )
 }
