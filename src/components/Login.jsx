ae8ec6e532dd5ac9ace04668e725b4dcbe940fd6
winwinchit1355 <winwinchit1355@gmail.com>
2023-11-08 16:44:44 +0630

    login Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import Nav from './Nav'
+import styled from 'styled-components'
 
+const SectionDiv=styled.section`
+  background-color:${(props)=> props.theme.colors.primary};
+  display:flex;
+  flex-dirction:column;
+  
+`;
+const InputStyle = styled.input`
+  display:block;
+`;
 export default function Login() {
   return (
     <div>
       <Nav />
       <h1>Login</h1>
-      <div>
+      <SectionDiv>
         <form action="">
-            <input type="tel" name="phone" />
-            <input type="password" name="pa" />
+            <label htmlFor="phone">Phone</label>
+            
+            <InputStyle type="tel" name="phone" />
+            
+            <label htmlFor="password">Password</label>
+            
+            <InputStyle type="password" name="password" />
         </form>
-      </div>
+      </SectionDiv>
     </div>
   )
 }
